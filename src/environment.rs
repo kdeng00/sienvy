e1e3a8bf8db63101a84465ffb6c8e3c66e9d9725
phoenix <mail@kundeng.us>
2026-07-07 16:31:40 -0400

    Removing async and async dev dependency

--- src/environment.rs
+++ src/environment.rs
@@ -1,4 +1,4 @@
-pub async fn get_db_url() -> crate::EnvVar {
+pub fn get_db_url() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::DB_URL;
     let value = std::env::var(key).expect(key);
@@ -6,7 +6,7 @@ pub async fn get_db_url() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_secret_main_key() -> crate::EnvVar {
+pub fn get_secret_main_key() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::SECRET_MAIN_KEY;
     let value = std::env::var(key).expect(key);
@@ -14,7 +14,7 @@ pub async fn get_secret_main_key() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_service_passphrase() -> crate::EnvVar {
+pub fn get_service_passphrase() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::SERVICE_PASSPHRASE;
     let value = std::env::var(key).expect(key);
@@ -22,7 +22,7 @@ pub async fn get_service_passphrase() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_secret_key() -> crate::EnvVar {
+pub fn get_secret_key() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::SECRET_KEY;
     let value = std::env::var(key).expect(key);
@@ -30,7 +30,7 @@ pub async fn get_secret_key() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_root_directory() -> crate::EnvVar {
+pub fn get_root_directory() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::ROOT_DIRECTORY;
     let value = std::env::var(key).expect(key);
@@ -38,7 +38,7 @@ pub async fn get_root_directory() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_icarus_base_api_url() -> crate::EnvVar {
+pub fn get_icarus_base_api_url() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::ICARUS_BASE_API_URL;
     let value = std::env::var(key).expect(key);
@@ -46,7 +46,7 @@ pub async fn get_icarus_base_api_url() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_icarus_auth_base_api_url() -> crate::EnvVar {
+pub fn get_icarus_auth_base_api_url() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::ICARUS_AUTH_BASE_API_URL;
     let value = std::env::var(key).expect(key);
@@ -54,7 +54,7 @@ pub async fn get_icarus_auth_base_api_url() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_app_env() -> crate::EnvVar {
+pub fn get_app_env() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::APP_ENV;
     let value = std::env::var(key).expect(key);
@@ -62,14 +62,14 @@ pub async fn get_app_env() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_backend_port() -> crate::EnvVar {
+pub fn get_backend_port() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::BACKEND_PORT;
     let value = std::env::var(key).expect(key);
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_frontend_url() -> crate::EnvVar {
+pub fn get_frontend_url() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::FRONTEND_URL;
     let value = std::env::var(key).expect(key);
@@ -77,7 +77,7 @@ pub async fn get_frontend_url() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_rust_log() -> crate::EnvVar {
+pub fn get_rust_log() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::RUST_LOG;
     let value = std::env::var(key).expect(key);
@@ -85,7 +85,7 @@ pub async fn get_rust_log() -> crate::EnvVar {
     crate::init_envvar(key, &value)
 }
 
-pub async fn get_allowed_origins() -> crate::EnvVar {
+pub fn get_allowed_origins() -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let key = crate::keys::ALLOWED_ORIGINS;
     let value = std::env::var(key).expect(key);
@@ -97,7 +97,7 @@ pub async fn get_allowed_origins() -> crate::EnvVar {
 }
 
 /// Get environment not specified in the code
-pub async fn get_env(environment: &str) -> crate::EnvVar {
+pub fn get_env(environment: &str) -> crate::EnvVar {
     dotenvy::dotenv().ok();
     let my_error = format!("{environment} {}", crate::keys::error::GENERAL_ERROR);
     let value = std::env::var(environment).expect(&my_error);
