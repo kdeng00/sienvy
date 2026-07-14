d06c8fdf49920cf4d8ba0487c20f5bdc851c36de
phoenix <mail@kundeng.us>
2026-07-14 16:33:01 -0400

    Name change (#3)
    Reviewed-on: http://git.kundeng.us/phoenix/sienvy/pulls/3

--- tests/test.rs
+++ tests/test.rs
@@ -2,7 +2,7 @@
 mod tests {
     #[test]
     fn test_dburl() {
-        let result = icarus_envy::environment::get_db_url();
+        let result = sienvy::environment::get_db_url();
         assert_eq!(
             result.value, "postgres://myuser:password@localhost:5432/my_db",
             "DATABASE_URL does not match {:?}",
@@ -12,7 +12,7 @@ mod tests {
 
     #[test]
     fn test_get_secret_main_key() {
-        let result = icarus_envy::environment::get_secret_main_key();
+        let result = sienvy::environment::get_secret_main_key();
         assert_eq!(
             result.value, "Somesupersecretpassword!!!45345435",
             "SECRET_MAIN_KEY does not match {:?}",
@@ -22,7 +22,7 @@ mod tests {
 
     #[test]
     fn test_get_service_passphrase() {
-        let result = icarus_envy::environment::get_service_passphrase();
+        let result = sienvy::environment::get_service_passphrase();
         assert_eq!(
             result.value, "T5OCHDHadAtuOWIqRAS7u8XHDDkzKT1Uvvw7mGMkNzKjVdlHA8xGdILf2adDHspO",
             "SERVICE_PASSPHRASE does not match {:?}",
@@ -32,7 +32,7 @@ mod tests {
 
     #[test]
     fn test_get_secret_key() {
-        let result = icarus_envy::environment::get_secret_key();
+        let result = sienvy::environment::get_secret_key();
         assert_eq!(
             result.value, "AmIGoodEnoughForYou?",
             "SECRET_KEY does not match {:?}",
@@ -42,7 +42,7 @@ mod tests {
 
     #[test]
     fn test_get_root_directory() {
-        let result = icarus_envy::environment::get_root_directory();
+        let result = sienvy::environment::get_root_directory();
         assert_eq!(
             result.value, "/path/to/root",
             "ROOT_DIRECTORY does not match {:?}",
@@ -51,68 +51,68 @@ mod tests {
     }
 
     #[test]
-    fn test_get_icarus_base_api_url() {
-        let result = icarus_envy::environment::get_icarus_base_api_url();
+    fn test_get_soaricarus_base_api_url() {
+        let result = sienvy::environment::get_soaricarus_base_api_url();
         assert_eq!(
-            result.value, "https://icarus.com",
-            "ICARUS_BASE_API_URL does not match {:?}",
+            result.value, "https://soaricarus.com",
+            "SOARICARUS_BASE_API_URL does not match {:?}",
             result
         )
     }
 
     #[test]
     fn test_get_app_env() {
-        let result = icarus_envy::environment::get_app_env();
+        let result = sienvy::environment::get_app_env();
         assert_eq!(
             result.value,
             "development",
             "{} does not match {:?}",
-            icarus_envy::keys::APP_ENV,
+            sienvy::keys::APP_ENV,
             result
         )
     }
 
     #[test]
     fn test_get_backend_port() {
-        let result = icarus_envy::environment::get_backend_port();
+        let result = sienvy::environment::get_backend_port();
         assert_eq!(
             result.value,
             "8001",
             "{} does not match {:?}",
-            icarus_envy::keys::BACKEND_PORT,
+            sienvy::keys::BACKEND_PORT,
             result
         )
     }
     #[test]
     fn test_get_frontend_url() {
-        let result = icarus_envy::environment::get_frontend_url();
+        let result = sienvy::environment::get_frontend_url();
         assert_eq!(
             result.value,
             "http://localhost:4200",
             "{} does not match {:?}",
-            icarus_envy::keys::FRONTEND_URL,
+            sienvy::keys::FRONTEND_URL,
             result
         )
     }
     #[test]
     fn test_get_rust_log() {
-        let result = icarus_envy::environment::get_rust_log();
+        let result = sienvy::environment::get_rust_log();
         assert_eq!(
             result.value,
             "debug",
             "{} does not match {:?}",
-            icarus_envy::keys::RUST_LOG,
+            sienvy::keys::RUST_LOG,
             result
         )
     }
     #[test]
     fn test_get_allowed_origins() {
-        let result = icarus_envy::environment::get_allowed_origins();
+        let result = sienvy::environment::get_allowed_origins();
         assert_eq!(
             result.value,
             "https://soaricarus.com,https://www.soaricarus.com",
             "{} does not match {:?}",
-            icarus_envy::keys::ALLOWED_ORIGINS,
+            sienvy::keys::ALLOWED_ORIGINS,
             result
         );
 
@@ -122,7 +122,7 @@ mod tests {
             result.key
         );
 
-        match icarus_envy::utility::delimitize(&result) {
+        match sienvy::utility::delimitize(&result) {
             Ok(allowed_origins) => {
                 assert_eq!(
                     allowed_origins.len(),
@@ -152,7 +152,7 @@ mod tests {
         ];
 
         for (key, value) in keys.iter() {
-            let result = icarus_envy::environment::get_env(key);
+            let result = sienvy::environment::get_env(key);
             assert_eq!(
                 result.value, *value,
                 "{:?} does not match {:?}",
